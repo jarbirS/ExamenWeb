28e46cf98f3af7a76d3a140147cf1fa6e3d58307
unknown <jarbirstewartmr@ufps.edu.co>
2020-06-18 09:12:50 -0500

    templates

--- src/main/java/com/examen/demo/entities/AccesoVisitante.java
+++ src/main/java/com/examen/demo/entities/AccesoVisitante.java
@@ -1,5 +1,58 @@
 package com.examen.demo.entities;
 
-public class AccesoVisitante {
+import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="accesovisitante")
+public class AccesoVisitante implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	@Id
+	private int id;
+	private int temperatura;
+	private Visitante visitante;
+	
+	public AccesoVisitante() {
+		
+	}
+	
+	
+	public AccesoVisitante(int id, int temperatura, Visitante visitante) {
+		super();
+		this.id = id;
+		this.temperatura = temperatura;
+		this.visitante = visitante;
+	}
+
+
+	public int getId() {
+		return id;
+	}
+
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+
+	public int getTeperatura() {
+		return temperatura;
+	}
+
+
+	public void setTemperatura(int teperatura) {
+		this.temperatura = teperatura;
+	}
+
+
+	public Visitante getVisitante() {
+		return visitante;
+	}
+
+
+	
 }
